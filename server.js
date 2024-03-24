90eda72f79d5d6cb8c45a63ec2793524346a72ac
Jetnik Syla <sjetnik@gmail.com>
2024-03-24 14:34:19 -0400

    added style css

---
+++ server.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+
+// stores cookies from user
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+const helpers = require("./utilis/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const { User, Blog, Comment } = require("./models");
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    // 24 hours
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// creates a session
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Now listening");
+  });
+});
