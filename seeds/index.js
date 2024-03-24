9a48748f341073e5f88b0da2d072f1f6597fcf53
Jetnik Syla <sjetnik@gmail.com>
2024-03-24 15:08:48 -0400

    rename seedComment file and added the index.js to the seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const seedUser = require("./seedUser");
+const seedBlog = require("./seedBlog");
+const seedComment = require(`./seedComment`);
+
+// Inserts seed entries in the table `users`
+
+
+const seedUsers = require('./user-seeds');
+const seedBlogs = require('./blog-seeds');
+const seedComments = require('./comment-seeds');
+
+const seedAll = async () => {
+    await seedUsers();
+    await seedBlogs();
+    await seedComments();
+    console.log('All seed data inserted successfully!');
+    process.exit(0);
+};
+
+seedAll();
